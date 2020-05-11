b125e283a514fafa25b9cc44563504488a91e0aa
Avaer Kazmer <a@modules.io>
2020-05-11 00:27:59 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const child_process = require('child_process');
+
+console.log('got env', process.env);
+
+const cp = child_process.spawn('npm', {
+  stdio: 'pipe',
+});
+cp.stdout.pipe(process.stdout);
+cp.stderr.pipe(process.stderr);
+cp.on('exit', (code, signal) => {
+  console.log('got code signal', code, signal);
+});

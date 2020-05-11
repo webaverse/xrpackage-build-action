7dc261c999bc4fe31d30287c5955bf3a90e27fc3
Avaer Kazmer <a@modules.io>
2020-05-11 01:42:54 -0400

    Action debugging

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
+const path = require('path');
 const child_process = require('child_process');
 
 console.log('got env', process.env);
 
-const cp = child_process.spawn('npm', {
+const cp = child_process.spawn('npm', [
+  'install',
+  'xrpk',
+], {
   stdio: 'pipe',
 });
 cp.stdout.pipe(process.stdout);
 cp.stderr.pipe(process.stderr);
 cp.on('exit', (code, signal) => {
-  console.log('got code signal', code, signal);
+  console.log('install done', code, signal);
 
-  const dst = code + '';
-  process.stdout.write(`::set-output name=dst::${dst}\n``);
+  const src = process.env['INPUT_SRC'];
+  console.log('building', path.join('node_modules', '.bin', 'xrpk'), [
+    'build',
+    src,
+  ]);
+  const cp2 = child_process.spawn(path.join('node_modules', '.bin', 'xrpk'), [
+    'build',
+    src,
+  ]);
+  cp2.stdout.pipe(process.stdout);
+  cp2.stderr.pipe(process.stderr);
+  cp2.on('exit', (code, signal) => {
+  	console.log('build done', code, signal);
+
+    const dst = code + '';
+    process.stdout.write(`::set-output name=dst::${dst}\n`);
+  });
 });

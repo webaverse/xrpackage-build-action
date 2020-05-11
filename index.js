ff03c3bdcfeaf4457854e778069b042003851007
Avaer Kazmer <a@modules.io>
2020-05-11 03:08:24 -0400

    Check status codes

--- index.js
+++ index.js
@@ -14,21 +14,31 @@ cp.stderr.pipe(process.stderr);
 cp.on('exit', (code, signal) => {
   console.log('install done', code, signal);
 
-  const src = path.join(process.env.GITHUB_WORKSPACE, process.env['INPUT_SRC']);
-  console.log('building', path.join('node_modules', '.bin', 'xrpk'), [
-    'build',
-    src,
-  ]);
-  const cp2 = child_process.spawn(path.join('node_modules', '.bin', 'xrpk'), [
-    'build',
-    src,
-  ]);
-  cp2.stdout.pipe(process.stdout);
-  cp2.stderr.pipe(process.stderr);
-  cp2.on('exit', (code, signal) => {
-  	console.log('build done', code, signal);
+  if (code === 0) {
+    const src = path.join(process.env.GITHUB_WORKSPACE, process.env['INPUT_SRC']);
+    console.log('building', path.join('node_modules', '.bin', 'xrpk'), [
+      'build',
+      src,
+    ]);
+    const cp2 = child_process.spawn(path.join('node_modules', '.bin', 'xrpk'), [
+      'build',
+      src,
+    ]);
+    cp2.stdout.on('data', d => {
+      bs.push(d);
+    });
+    cp2.stderr.pipe(process.stderr);
+    cp2.on('exit', (code, signal) => {
+      console.log('build done', code, signal);
 
-    const dst = code + '';
-    process.stdout.write(`::set-output name=dst::${dst}\n`);
-  });
+      if (code === 0) {
+        const dst = code + '';
+        process.stdout.write(`::set-output name=dst::${dst}\n`);
+      } else {
+        throw new Error(`invalid build status code: ${code}`);
+      }
+    });
+  } else {
+    throw new Error(`invalid install status code: ${code}`);
+  }
 });
